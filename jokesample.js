f987896c756f9c4949d6c3f2d9d8f28df4e23112
justcallmetj <58446365+justcallmetj@users.noreply.github.com>
2019-12-09 15:39:23 -0600

    Updated Ver 2.1

--- jokesample.js
+++ jokesample.js
@@ -1,7 +1,7 @@
 $('.btn').click(function() 
 {
   
-	$('.text').text('loading . . .');
+	$('.content').text('loading . . .');
 	//var url = ;
 	var pokeElem = document.getElementById("pokemon");
 	var filter = "";
@@ -28,23 +28,24 @@ $('.btn').click(function()
 	$.ajax(settings).done(function (response) 
 	{
 		console.log(response);
-		$('.text').text(JSON.stringify(response));
+		$('.content').text(JSON.stringify(response));
 		
-		var htmlString = "Name = " + response.name.toUpperCase() + "<br>";
-		htmlString += "Pokemon ID = " + response.id + "<br>";
-		htmlString += "Primary Type = " + response.types[0].type.name.toUpperCase() + "<br>";
+		var htmlString = "# " + response.id + "   ";
+		htmlString += response.name.toUpperCase() + "<br>";
+		htmlString += "Type: " + response.types[0].type.name.toUpperCase() ;
 			if (response.types[1] != null)
 			{
-				htmlString += "Secondary Type = " + response.types[1].type.name.toUpperCase() + "<br>"; 
+				htmlString += "Type: " + response.types[1].type.name.toUpperCase(); 
 			}
-		htmlString += "HP: " + response.stats[5].base_stat + "<br>";
+		htmlString += "<br />"
+		htmlString += "HP: " + response.stats[5].base_stat + "   ";
 		htmlString += "Attack: " + response.stats[4].base_stat + "<br>";
-		htmlString += "Defense: " + response.stats[3].base_stat + "<br>";
+		htmlString += "Defense: " + response.stats[3].base_stat + "   ";
 		htmlString += "Speical Attack: " + response.stats[2].base_stat + "<br>";
-		htmlString += "Special Defense: " + response.stats[1].base_stat + "<br>";
+		htmlString += "Special Defense: " + response.stats[1].base_stat + "   ";
 		htmlString += "Speed: " + response.stats[0].base_stat;
 		
-		$('.text').html(htmlString);		
+		$('.content').html(htmlString);		
 	});  
 });
 
